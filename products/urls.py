5e377c5b49fac7a49e44e841b39d03efc44339e5
Aria-vero-s <ariane.saulnier@gmail.com>
2023-03-23 19:51:40 +0000

    Product Admin - add product view, url and template

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('quote', views.quote, name='quote'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -29,3 +29,21 @@ def quote(request):
 
     return render(request, 'products/quote.html', context)
 
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
