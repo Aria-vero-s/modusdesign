e3a6948dbe323111716547ef01937228b12ea1d7
Aria-vero-s <ariane.saulnier@gmail.com>
2023-03-23 11:26:53 +0000

    created checkout app and models

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Quote, OrderItem, Order
+from .models import Product, Category, OrderItem, Order
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
