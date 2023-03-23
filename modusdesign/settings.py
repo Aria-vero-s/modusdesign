7711c43384f6dbb220624bd2566bbc861177bc80
Aria-vero-s <ariane.saulnier@gmail.com>
2023-03-23 11:40:46 +0000

    Added checkout views and templates

--- modusdesign/settings.py
+++ modusdesign/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'modusdesign.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,6 +79,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',#required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
