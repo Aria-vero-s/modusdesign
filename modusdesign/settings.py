94a2b466a66fabb8586b1b799cd944c128ca1465
Ariane <99185542+Aria-vero-s@users.noreply.github.com>
2023-04-25 08:04:59 +0100

    Deployment to heroku

--- modusdesign/settings.py
+++ modusdesign/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dnjkt0hvid688d(+t)+(t#+k8m3klxfb7bcc_#f-^j4*6!%3j0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
