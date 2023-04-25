5476c1389a9e19fb34cc8844245d213cabdb2c8b
Ariane <99185542+Aria-vero-s@users.noreply.github.com>
2023-04-25 22:42:04 +0100

    deployment git push heroku main

--- modusdesign/settings.py
+++ modusdesign/settings.py
@@ -183,19 +183,19 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'https://res.cloudinary.com/dpvdjusy5/image/upload/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+MEDIA_URL = 'https://res.cloudinary.com/dpvdjusy5/image/upload/'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
