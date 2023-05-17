25251314306d8876453b8d958bed514d3624a066
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-05-17 20:49:16 +0100

    Updated secret key for WH

--- modusdesign/settings.py
+++ modusdesign/settings.py
@@ -255,7 +255,7 @@ DEFAULT_FROM_EMAIL = 'modusdesign.official@gmail.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'ariane.saulnier@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
