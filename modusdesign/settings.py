0e0e060d084d64606d587c18f2de6d8ab050821b
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-05-17 19:46:37 +0100

    Added cache control

--- modusdesign/settings.py
+++ modusdesign/settings.py
@@ -178,6 +178,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'modusdesign'
     AWS_S3_REGION_NAME = 'EU (London) eu-west-2'
