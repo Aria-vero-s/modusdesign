39bcbae2477848598a3f62e30e3e1aca6d96cf12
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-05-18 20:05:01 +0100

    Deployment

--- modusdesign/settings.py
+++ modusdesign/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 if os.path.exists('env.py'):
     import env
 
+# Import necessary packages
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -49,7 +51,6 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
-    'cloudinary',
 
     # Other
     'crispy_forms',
@@ -219,23 +220,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MEDIA_URL = '/media/'
-
-# MEDIA_URL = 'https://res.cloudinary.com/dpvdjusy5/image/upload/'
-
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': 'dpvdjusy5',
-#     'API_KEY': 'aOd1laIemjcIrunnpa-TNY5rIoA',
-#     'API_SECRET': 'aOd1laIemjcIrunnpa-TNY5rIoA',
-# }
 
 
 # Default primary key field type
