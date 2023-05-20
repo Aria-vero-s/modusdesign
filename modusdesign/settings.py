85144fd18bc4a7efeb968e75461570950131780b
aria-vero-s <99185542+Aria-vero-s@users.noreply.github.com>
2023-05-20 15:59:31 +0100

    New AWS bucket name update

--- modusdesign/settings.py
+++ modusdesign/settings.py
@@ -185,7 +185,7 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'modusdesign'
+    AWS_STORAGE_BUCKET_NAME = 'modus-design'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
